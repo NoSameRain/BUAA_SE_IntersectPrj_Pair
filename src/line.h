ac020e1c80d60c63f11ea54805c31fdb6046c84b
NNNNNF <17373532@buaa.edu.cn>
2020-03-19 16:30:32 +0800

    save SS and Point bug

--- src/line.h
+++ src/line.h
@@ -5,6 +5,7 @@
 #include <string>
 #include<sstream>
 #include <fstream>
+#include <set>
 #include "stdlib.h"
 using namespace std;
 class line
@@ -23,10 +24,45 @@ public:
 	void store_coor(string str);
 };
 
+class Point {
+public:
+	double Xpoint = 0;
+	double Ypoint = 0;
+
+public:
+	Point() {}
+	Point(double x, double y) {
+		Xpoint = x;
+		Ypoint = y;
+	}
+	string showPoint();
+	bool operator<(const Point& a)const
+	{
+		if (Xpoint != a.Xpoint) {
+			return Xpoint < a.Xpoint;
+		}
+		else {
+			return Ypoint < a.Ypoint;
+		}
+	}
+
+	bool operator == (const Point& p) const
+	{
+		if (Xpoint != p.Xpoint) {
+			return false;
+		}
+		else {
+			return Ypoint == p.Ypoint;
+		}
+	}
+};
+
 extern map<string, int> intersection; //所有交点
 extern vector < line > coor_4_line; //每条线的坐标
 extern vector < vector<double> > new_inter;
+extern set<Point> points;
 extern int p_cnt, N;
+
 void cnt_coor_num(); //计算交点个数
 void calcu_coor(int i, int j);
 void test();
@@ -36,3 +72,4 @@ int JudgeType(int i, int j, double x, double y);
 int JudgeRadial(int i, double x,double y);
 int JudgeSegment(int i, double x, double y);
 void Dealwith(line l1, line l2);
+void DealwithKexist(line l1, line l2);
